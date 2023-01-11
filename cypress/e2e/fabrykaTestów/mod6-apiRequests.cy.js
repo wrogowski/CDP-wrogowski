3b19183205754466c74b5e4739b0badc94ff494d
W R <wrogowski@pgs-soft.com>
2023-01-12 00:43:10 +0100

    put and deleted methods added

--- cypress/e2e/fabrykaTestów/mod6-apiRequests.cy.js
+++ cypress/e2e/fabrykaTestów/mod6-apiRequests.cy.js
@@ -1,13 +1,18 @@
 import { expect } from "chai";
 
-const url = "https://jsonplaceholder.typicode.com/";
+const url = "https://jsonplaceholder.typicode.com/posts/";
+const firstPostUrl = url + 1;
+const requestBody = {
+  userID: 15,
+  title: "My first post",
+  body: "This is my first post!",
+};
 
 describe("json api test", () => {
-  const postsUrl = `${url}posts`;
-
   it("GET all posts", () => {
-    cy.request(postsUrl).then((response) => {
+    cy.request(url).then((response) => {
       const body = response.body;
+
       expect(body).to.be.an("array");
       expect(body.length).to.eq(100);
       expect(response.status).to.eq(200);
@@ -15,15 +20,9 @@ describe("json api test", () => {
   });
 
   it("POST a post", () => {
-    const requestBody = {
-      userID: 15,
-      title: "My first post",
-      body: "some post body",
-    };
-
     cy.request({
       method: "POST",
-      url: postsUrl,
+      url: url,
       body: requestBody,
       headers: {
         "Accept-Language": "en-us",
@@ -33,12 +32,51 @@ describe("json api test", () => {
       const body = response.body;
 
       expect(response.status).to.eq(201);
-
-      const requestBody = {
-        userID: 15,
-        body: "some postp body",
-      };
       expect(body).to.include(requestBody);
+    });
+  });
+
+  it("PUT updates a post", () => {
+    const firstPostInitialContent = cy
+      .request(firstPostUrl)
+      .then((response) => {
+        const body = response.body;
+
+        expect(response.status).to.eq(200);
+        expect(body).to.be.an("object");
+        expect(body).not.to.include(requestBody);
+      });
+
+    cy.request({
+      method: "PUT",
+      url: firstPostUrl,
+      body: requestBody,
+      headers: {
+        "Accept-Language": "en-us",
+        "Conent-Type": "application/json",
+      },
+    }).then((response) => {
+      const body = response.body;
+
+      expect(response.status).to.eq(200);
+      expect(body).to.include(requestBody);
+      expect(body).not.to.include(firstPostInitialContent);
+    });
+  });
+
+  it("DELETE a post", () => {
+    cy.request({
+      method: "DELETE",
+      url: firstPostUrl,
+      headers: {
+        "Accept-Language": "en-us",
+        "Conent-Type": "application/json",
+      },
+    }).then((response) => {
+      const currentDateUTC = new Date().toUTCString();
+
+      expect(response.status).to.eq(200);
+      expect(response.headers).to.have.property("date", currentDateUTC);
     });
   });
 });
